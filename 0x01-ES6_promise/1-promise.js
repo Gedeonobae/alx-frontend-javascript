b6287e5b6bbdfe3dfdfd34b5421abf8293d67d1c
Gedeon Obae Gekonge <85337093+Gedeonobae@users.noreply.github.com>
2022-10-26 23:56:23 +0300

    return a promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: "Success",
+      });
+    } else {
+      reject(new Error("The fake API is not working currently"));
+    }
+  });
+}
