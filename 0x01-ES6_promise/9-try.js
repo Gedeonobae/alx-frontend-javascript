3946d8f2c381c6f748af6af6dc45a8e7dcfde3b2
Gedeon Obae Gekonge <85337093+Gedeonobae@users.noreply.github.com>
2022-11-04 21:34:36 +0300

    Write a function named guardrail that will accept one argument mathFunction (Function).

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(err.toString());
+  } finally {
+    queue.push("Guardrail was processed");
+  }
+  return queue;
+}
