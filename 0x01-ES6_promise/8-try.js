07e4a48e8b67564d3311d9a4370334f54d122a3c
Gedeon Obae Gekonge <85337093+Gedeonobae@users.noreply.github.com>
2022-11-04 21:15:24 +0300

    Write a function named divideFunction that will accept two arguments: numerator (Number) and denominator (Number).

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error("cannot divide by 0");
+  }
+  return numerator / denominator;
+}
