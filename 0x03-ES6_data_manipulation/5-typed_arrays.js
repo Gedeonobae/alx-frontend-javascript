3a9b5f474995808c5dd0efdc96ec0330388c3ae6
Gedeon Obae Gekonge <85337093+Gedeonobae@users.noreply.github.com>
2022-11-28 23:56:47 +0300

    upload 5-typed_arrays.js file

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer, 0);
+
+  if (position > length - 1) {
+    throw Error('Position outside range');
+  } else {
+    view.setInt8(position, value);
+  }
+  return view;
